831ab7f5ce69e74cda9cbd63d3c87034f9b93f88
selim.toksal <selim.toksal@kariyer.net>
2019-02-21 14:33:59 +0300

    fist and last item line fix

--- app/src/main/java/com/selimtoksal/imagedtimelineview/ImagedTimelineView.kt
+++ app/src/main/java/com/selimtoksal/imagedtimelineview/ImagedTimelineView.kt
@@ -20,11 +20,13 @@ constructor(context: Context, attrs: AttributeSet?) : LinearLayout(context, attr
 
     private val paint: Paint = Paint()
     private var lineColor: Int? = null
-    @DrawableRes var image: Drawable? = null
+    @DrawableRes
+    var image: Drawable? = null
     private var headerText: String? = null
     private var descriptionText: String? = null
     private var imagePadding: Int? = null
     private var isLastItem: Boolean = false
+    private var isFirstItem: Boolean = false
 
     private lateinit var imageView: ImageView
 
@@ -47,10 +49,11 @@ constructor(context: Context, attrs: AttributeSet?) : LinearLayout(context, attr
         this.headerText = typedArray.getString(R.styleable.ImagedTimelineView_header_text)
         this.descriptionText = typedArray.getString(R.styleable.ImagedTimelineView_description_text)
         this.imagePadding = typedArray.getInt(R.styleable.ImagedTimelineView_image_padding, 0)
-        this.isLastItem = typedArray.getBoolean(R.styleable.ImagedTimelineView_is_last_item,false)
+        this.isLastItem = typedArray.getBoolean(R.styleable.ImagedTimelineView_is_last_item, false)
+        this.isFirstItem = typedArray.getBoolean(R.styleable.ImagedTimelineView_is_first_item, false)
     }
 
-    private fun initViews(){
+    private fun initViews() {
         this.orientation = LinearLayout.HORIZONTAL
         this.gravity = Gravity.CENTER_VERTICAL
 
@@ -75,12 +78,19 @@ constructor(context: Context, attrs: AttributeSet?) : LinearLayout(context, attr
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        if(!isLastItem) {
-            val p = Paint(Paint.ANTI_ALIAS_FLAG)
-            val stopY = this.height
-            val startX = this.imageView.width / 2
-            val startY = this.imageView.height
-            canvas.drawLine(startX.toFloat(), 0F, startX.toFloat(), stopY.toFloat(), p)
+
+        val p = Paint(Paint.ANTI_ALIAS_FLAG)
+        val stopY = this.height
+        val startX = this.imageView.width / 2
+        val stopX = this.imageView.width / 2
+        val startY = this.imageView.height / 2
+
+        if (!isFirstItem) {
+            canvas.drawLine(startX.toFloat(), 0F, stopX.toFloat(), stopY.toFloat() / 2, p)
+        }
+
+        if (!isLastItem) {
+            canvas.drawLine(startX.toFloat(), startY.toFloat(), stopX.toFloat(), stopY.toFloat(), p)
         }
     }
 
